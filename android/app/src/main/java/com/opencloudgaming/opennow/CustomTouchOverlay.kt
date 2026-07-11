d90c79a737c00fc3a1bec5fd19c11647bbe3544f
kalamota99-collab <kalamota99@gmail.com>
2026-07-12 02:44:06 +0530

    TungTungTungSahur

--- android/app/src/main/java/com/opencloudgaming/opennow/CustomTouchOverlay.kt
+++ android/app/src/main/java/com/opencloudgaming/opennow/CustomTouchOverlay.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.serialization.Serializable
 import kotlin.math.sqrt
 
